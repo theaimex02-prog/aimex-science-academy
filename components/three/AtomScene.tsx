0df79c90d85b59b0d6462ae619fe4a2b520a2d10
myrofficial786-cmd <myrofficial786@gmail.com>
2026-07-26 14:05:19 +0530

    Complete Aimex website with responsive improvements

--- components/three/AtomScene.tsx
+++ components/three/AtomScene.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Canvas, useFrame } from "@react-three/fiber";
-
+import { Float } from "@react-three/drei";
 import { useRef } from "react";
 import * as THREE from "three";
 
@@ -81,7 +81,12 @@ function Atom() {
   });
 
   return (
-  <group ref={group}>
+    <Float
+      speed={2}
+      rotationIntensity={0.15}
+      floatIntensity={0.8}
+    >
+      <group ref={group}>
         {/* Nucleus */}
         <mesh>
           <sphereGeometry args={[0.32, 32, 32]} />
@@ -155,7 +160,7 @@ function Atom() {
           color="#93c5fd"
         />
       </group>
-    
+    </Float>
   );
 }
 
